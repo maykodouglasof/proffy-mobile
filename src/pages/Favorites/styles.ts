d09311ae232d2fbb3d844bf9127fe4dc47b95ab9
Mayko Douglas <maykodouglasbr@outlook.com>
2020-08-07 16:32:57 -0300

    Update Api and Pages

--- src/pages/Favorites/styles.ts
+++ src/pages/Favorites/styles.ts
@@ -1,12 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
   container: {
-    backgroundColor: '#f0f0f7',
     flex: 1,
+    backgroundColor: "#f0f0f7",
   },
 
+  teacherList: {
+    marginTop: -40,
+  },
 });
 
 export default styles;
